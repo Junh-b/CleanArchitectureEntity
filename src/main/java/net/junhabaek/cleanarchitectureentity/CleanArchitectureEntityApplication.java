f7e84b106c3ad70a467e9ec10fe7c5c66e7a3330
Junh <jooha208@gmail.com>
2022-09-14 09:38:40 +0900

    initial settings

---
+++ src/main/java/net/junhabaek/cleanarchitectureentity/CleanArchitectureEntityApplication.java
@@ -0,0 +1,11 @@
+package net.junhabaek.cleanarchitectureentity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleanArchitectureEntityApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CleanArchitectureEntityApplication.class, args);
+    }
+}
