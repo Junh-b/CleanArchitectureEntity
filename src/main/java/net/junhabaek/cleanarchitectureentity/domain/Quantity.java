2761d39d14fbc4e286c7f3d12b8845b3d76af590
Junh <jooha208@gmail.com>
2022-09-14 12:28:42 +0900

    fix method interface of minus, multiply in Quantity Value Object

--- src/main/java/net/junhabaek/cleanarchitectureentity/domain/Quantity.java
+++ src/main/java/net/junhabaek/cleanarchitectureentity/domain/Quantity.java
@@ -15,11 +15,11 @@ public class Quantity {
     public Quantity plus(Quantity other){
         return new Quantity(this.quantity.add(other.quantity));
     }
-    public Money minus(Quantity other) {
-        return new Money(this.quantity.subtract(other.quantity));
+    public Quantity minus(Quantity other) {
+        return new Quantity(this.quantity.subtract(other.quantity));
     }
-    public Money multiply(Long times) {
-        return new Money(this.quantity.multiply(BigInteger.valueOf(times)));
+    public Quantity multiply(Long times) {
+        return new Quantity(this.quantity.multiply(BigInteger.valueOf(times)));
     }
 
     private Quantity(){
